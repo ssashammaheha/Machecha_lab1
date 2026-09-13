32ecd9a6816afe050016d7a42a87bc5d31af886b
ssashammaheha <ssashammaheha@gmail.com>
2026-09-13 12:35:24 +0300

    truba 1 vivod

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -18,10 +18,42 @@ struct CompressorStation
     int stationClass;
 };
 
+void showPipe(Pipe pipe)
+{
+    std::cout << "name: " << pipe.name << std::endl;
 
+    std::cout << "lenght: "
+        << pipe.length
+        << " km"
+        << std::endl;
+
+    std::cout << "diametr: "
+        << pipe.diameter
+        << " mm"
+        << std::endl;
+
+    std::cout << "is repair: ";
+
+    if (pipe.inRepair)
+    {
+        std::cout << "Да";
+    }
+    else
+    {
+        std::cout << "Нет";
+    }
+}
 
 
 int main()
 {
+    Pipe pipe1;
+    pipe1.name = "truba 1";
+    pipe1.length = 15.5;
+    pipe1.diameter = 520;
+    pipe1.inRepair = false;
 
+
+    showPipe(pipe1);
+    
 }
